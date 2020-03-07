5519c9c7148883d51b8ba4d7411eba77b563017c
thePmSquare <impmmmm@gmail.com>
2020-03-07 20:35:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.get("/", (req, res) => res.render("landing"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
