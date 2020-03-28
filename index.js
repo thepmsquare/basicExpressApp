ea3fd0e4472e3bf4a853976bd4905b142f6ab607
thePmSquare <impmmmm@gmail.com>
2020-03-28 21:38:12 +0530

    link css and js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("landing"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
